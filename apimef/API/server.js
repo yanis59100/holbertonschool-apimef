185c6594a709926184eea0a4b2d4923624933db7
yanis59100 <meftouh.yanis@hotmail.com>
2024-12-27 19:38:11 +0100

    Modify paths API

--- apimef/API/server.js
+++ apimef/API/server.js
@@ -3,30 +3,36 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.get('/index.html', (req, res) => {
+// Route pour la page d'accueil (index.html)
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/html', 'index.html'));
 });
 
-app.get('/boutique.html', (req, res) => {
+// Route pour /boutique qui doit servir boutique.html
+app.get('/boutique', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/html', 'boutique.html'));
 });
 
-app.get('/connexion.html', (req, res) => {
+// Route pour /connexion qui sert connexion.html
+app.get('/connexion', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/html', 'connexion.html'));
 });
 
-app.get('/inscription.html', (req, res) => {
+// Route pour /inscription qui sert inscription.html
+app.get('/inscription', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/html', 'inscription.html'));
 });
 
-app.get('/images.html', (req, res) => {
+// Route pour /images qui sert images.html
+app.get('/images', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/html', 'images.html'));
 });
 
+// Serveur les fichiers statiques (CSS, JS, images)
 app.use('/apimef/frontend', express.static(path.join(__dirname, '../frontend')));
-
 app.use('/apimef/img', express.static(path.join(__dirname, '../img')));
 
+// Lancer le serveur sur le port 3000
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
